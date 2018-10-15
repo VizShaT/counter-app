80d81603f854a057ef831a53966fd6eb60984740
kikit <kikit@users.noreply.github.com>
2018-10-15 20:42:58 +0530

    03_07 Rendering Classes Dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,6 +11,10 @@ class Counter extends Component {
     fontWeight: "bold"
   };
   render() {
+    {
+      /*let classes = "badge m-2 badge-";
+      classes += this.state.count === 0 ? "warning" : "primary"; */
+    }
     return (
       <div>
         {/* <img src={this.state.imageUrl} /> */}
@@ -20,11 +24,18 @@ class Counter extends Component {
         {/*<span style={{ fontSize: 100 }} className="badge badge-primary m-2">
           {this.formatCount()}
         </span>*/}
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
